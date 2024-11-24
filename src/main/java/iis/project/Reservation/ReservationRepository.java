25a3f4120b56dff6f0f8da371fb559b64d648f26
Artem Dvorychanskyi <xdvory00@stud.fit.vutbr.cz>
2024-11-24 03:34:25 +0100

    method for find future Reservations fixed

--- src/main/java/iis/project/Reservation/ReservationRepository.java
+++ src/main/java/iis/project/Reservation/ReservationRepository.java
@@ -13,5 +13,6 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     boolean existsByStartDateTimeLessThanEqualAndEndDateTimeGreaterThanEqualAndStatusAndDeviceId(LocalDateTime startDateTime, LocalDateTime endDateTime, ReservationStatus status, Long DeviceId);
     List<Reservation> findAllByStartDateTimeLessThanEqualAndEndDateTimeGreaterThanEqualAndDevice(LocalDateTime startDateTime, LocalDateTime endDateTime, Device device);
     List<Reservation> findAllByUserId(Long userId);
-    List<Reservation> findAllByStartDateTimeLessThanEqualAndDeviceId(LocalDateTime startDateTime, Long deviceId);
+    List<Reservation> findAllByStartDateTimeGreaterThanEqualAndDeviceId(LocalDateTime startDateTime, Long deviceId);
+
 }
