abeccc499e1703f8e337eb61bd8406ad25fae54d
Artem Dvorychanskyi <xdvory00@stud.fit.vutbr.cz>
2024-11-25 18:34:02 +0100

    small fixes

--- src/main/java/iis/project/DeviceHours/DayOfWeek.java
+++ src/main/java/iis/project/DeviceHours/DayOfWeek.java
@@ -1,17 +1,20 @@
 package iis.project.DeviceHours;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum DayOfWeek {
+    @JsonProperty("1")
     MONDAY,
-
+    @JsonProperty("2")
     TUESDAY,
-
+    @JsonProperty("3")
     WEDNESDAY,
-
+    @JsonProperty("4")
     THURSDAY,
-
+    @JsonProperty("5")
     FRIDAY,
-
+    @JsonProperty("6")
     SATURDAY,
-
+    @JsonProperty("7")
     SUNDAY
 }
